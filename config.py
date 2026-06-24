7c0e343aa31f1084a3ea8af1acba88f9eb61029d
gbriellopes <gabriellopes0603@gmail.com>
2026-06-24 12:17:41 -0300

    adicionando alfabetização

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 CAMINHO_BANCO = os.path.join(BASE_DIR, 'bh_censo.duckdb')
 CAMINHO_CSV_GEO = os.path.join(BASE_DIR, 'setores_MG.csv')
-CAMINHO_CSV_DEMOGRAFIA = os.path.join(BASE_DIR, 'Agregados_por_setores_demografia_BR.csv') # dados de demografia
+CAMINHO_CSV_DEMOGRAFIA = os.path.join(BASE_DIR, 'Agregados_por_setores_demografia_BR.csv')
+CAMINHO_CSV_ALFABETIZACAO =  os.path.join(BASE_DIR, 'Agregados_por_setores_alfabetizacao_BR.csv')
 CAMINHO_SCHEMA_SQL = os.path.join(BASE_DIR, 'schema.sql')
 
 # código IBGE para Belo Horizonte
--- schema.sql
+++ schema.sql
@@ -24,5 +24,34 @@ CREATE TABLE IF NOT EXISTS indicador_populacao (
     cd_setor VARCHAR PRIMARY KEY REFERENCES setor_censitario(cd_setor),
     pop_total INTEGER,
     pop_masc INTEGER,
-    pop_fem INTEGER
+    pop_fem INTEGER,
+    pop_0_4 INTEGER,
+    pop_5_9 INTEGER,
+    pop_10_14 INTEGER,
+    pop_15_19 INTEGER,
+    pop_20_24 INTEGER,
+    pop_25_29 INTEGER,
+    pop_30_39 INTEGER,
+    pop_40_49 INTEGER,
+    pop_50_59 INTEGER,
+    pop_60_69 INTEGER,
+    pop_70_mais INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS indicador_alfabetizacao (
+    cd_setor VARCHAR PRIMARY KEY REFERENCES setor_censitario(cd_setor),
+
+    -- alfabetizados/não alfabetizados por faixa etária
+    alf_15_29 INTEGER,
+    nao_alf_15_29 INTEGER,
+    alf_30_59 INTEGER,
+    nao_alf_30_59 INTEGER,
+    alf_60_mais INTEGER,
+    nao_alf_60_mais INTEGER,
+
+    -- totais por sexo (+15)
+    total_masc_alf INTEGER,
+    total_masc_nao INTEGER,
+    total_fem_alf INTEGER,
+    total_fem_nao INTEGER
 );
